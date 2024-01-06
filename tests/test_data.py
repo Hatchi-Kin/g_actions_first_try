d97d02219cfd09eeb1613693ff5a20f0e3ed484d
Hatchi-Kin <j.needham@hotmail.fr>
2024-01-06 21:18:28 +0100

    11

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,33 @@
-from connection import Connection
-# from connection import Connection
+import mysql.connector as mysql
+
+class Connection:
+    __USER = 'root'
+    __PWD = 'example'
+    __HOST = 'localhost'
+    __PORT = '3306'
+    __DB = 'testdb'
+    __cursor = None
+    __bdd = None
+
+
+    @classmethod
+    def connect(cls):
+        if cls.__cursor == None : 
+            cls.__bdd = mysql.connect(user = cls.__USER, password = cls.__PWD, host = cls.__HOST, port = cls.__PORT, database = cls.__DB)
+            cls.__cursor = cls.__bdd.cursor() 
+        
+        return cls.__cursor
+    
+
+    @classmethod
+    def disconnect(cls):
+        # indispensable pour closer, il faut que tout le cursor soit lu
+        if cls.__bdd.in_transaction:
+            cls.__bdd.commit()
+        cls.__cursor.fetchall()
+        cls.__cursor.close()
+        cls.__bdd.close()
+        cls.__cursor = None
 
 
 def create_test_table():
